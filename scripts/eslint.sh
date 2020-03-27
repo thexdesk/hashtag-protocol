4340f430d576583cfd1222925258b660a36fef31
Michael Paler <paler@civicsolar.com>
2020-03-27 16:39:18 -0700

    Adjust global file matching param in eslint.sh.
    gh-5

--- scripts/eslint.sh
+++ scripts/eslint.sh
@@ -3,4 +3,4 @@
 # Intended to be run from the project (repository) root
 # (eg. "./scripts/eslint.sh").
 
-./.platform/local/deps/nodejs/node_modules/.bin/eslint --max-warnings 0 "$@" .
+./.platform/local/deps/nodejs/node_modules/.bin/eslint --max-warnings 0 "src/**"
