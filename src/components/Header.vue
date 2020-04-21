b58719058fe260358586f23317be9d3a79610db8
Michael Paler <paler@civicsolar.com>
2020-04-21 16:22:00 -0700

    Lint components.
    gh-17

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,44 +1,59 @@
 <template>
-    <b-navbar>
-        <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="../assets/logo.svg"
-                    alt="Content tagging for the decentralized web"
-                /> <h1>Hashtag</h1>
-            </b-navbar-item>
-        </template>
-        <template slot="end">
-            <b-navbar-item tag="div">
-               <div>
-                <b-field>
-                  <b-select
-                      placeholder="ETH"
-                      expanded>
-                      <option value="eth">ETH</option>
-                      <option value="usd">USD</option>
-                      <option value="eur">EUR</option>
-                      <option value="gbp">GBP</option>
-                  </b-select>
-                </b-field>
-               </div>
-            </b-navbar-item>
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-primary is-outlined">
-                        {{ account | shortEth }}
-                    </a>
-                </div>
-            </b-navbar-item>
-        </template>
-    </b-navbar>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="../assets/logo.svg"
+          alt="Content tagging for the decentralized web"
+        />
+        <h1>Hashtag</h1>
+      </b-navbar-item>
+      <a
+        href="https://github.com/buefy/buefy"
+        target="_blank"
+        title="Github"
+        class="navbar-item"
+      >
+        <span class="icon"><i class="mdi mdi-github-circle mdi-24px"></i></span>
+      </a>
+      <a
+        href="https://discordapp.com/invite/ZkdFJMr"
+        target="_blank"
+        title="Discord"
+        class="navbar-item"
+      >
+        <span class="icon"><i class="mdi mdi-discord mdi-24px"></i></span>
+      </a>
+    </template>
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div>
+          <b-field>
+            <b-select placeholder="ETH" expanded>
+              <option value="eth">ETH</option>
+              <option value="usd">USD</option>
+              <option value="eur">EUR</option>
+              <option value="gbp">GBP</option>
+            </b-select>
+          </b-field>
+        </div>
+      </b-navbar-item>
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-primary is-outlined">
+            {{ account | shortEth }}
+          </a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
+import { mapGetters } from "vuex";
 
 export default {
-  name: "header",
+  name: "Header",
   computed: mapGetters(["account"]),
 };
 </script>
