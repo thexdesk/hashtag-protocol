2c73d2cb6088d6adb9b64490dc474defe7db4b2b
Andy Gray <andy.gray@blockrocket.tech>
2020-07-07 16:12:28 +0100

    gh-63 added ordering on NFT details so tags in correct order

--- hashtag-dapp/src/queries.js
+++ hashtag-dapp/src/queries.js
@@ -131,7 +131,7 @@ query hashtagsByName($name: String!) {
 
 export const TAGS_BY_DIGITAL_ASSET = gql(`
    query tagsByDigitalAsset($nftContract: String!, $nftId: String!) {
-    tagsByDigitalAsset: tags(where:{nftContract: $nftContract, nftId: $nftId}) {
+    tagsByDigitalAsset: tags(where:{nftContract: $nftContract, nftId: $nftId}, orderBy: timestamp, orderDirection: desc) {
     id
     nftContract
     nftContractName
