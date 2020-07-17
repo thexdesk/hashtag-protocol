ae8c5740bb415211a53587de87193c6dce877499
Michael Paler <paler@civicsolar.com>
2020-07-17 09:18:21 -0400

    Tweak docs migration scripts.
    gh-45

--- hashtag-dapp/scripts/pull-docs.sh
+++ hashtag-dapp/scripts/pull-docs.sh
@@ -10,10 +10,10 @@
 # If we are building on platform.sh
 if [ -d /app/.global ]; then
   # Copy documentation files from network drive to assets folder.
-  cp -R /app/network/docs/ /app/src/assets/docs
-  cp -R /app/network/docs/ /app/src/assets/docs
+  cp -R /app/network/docs/. /app/src/assets/docs/
+  cp -R /app/network/docs/. /app/src/assets/docs/
 else
   # We are building locally, just copy right from the hashtag-docs folder.
-  cp -R ./../hashtag-docs/docs/guide/faqs/ ./src/assets/docs
-  cp -R ./../hashtag-docs/docs/guide/help/ ./src/assets/docs
+  cp -R ./../hashtag-docs/docs/guide/faqs/. ./src/assets/docs/
+  cp -R ./../hashtag-docs/docs/guide/help/. ./src/assets/docs/
 fi
--- hashtag-docs/scripts/platformsh/push-docs.sh
+++ hashtag-docs/scripts/platformsh/push-docs.sh
@@ -1,3 +1,3 @@
 # Move raw markdown documents to network drive.
-cp -R /app/docs/guide/faqs/ /app/network/docs
-cp -R /app/docs/guide/help/ /app/network/docs
+cp -R /app/docs/guide/faqs/. /app/network/docs/
+cp -R /app/docs/guide/help/. /app/network/docs/
