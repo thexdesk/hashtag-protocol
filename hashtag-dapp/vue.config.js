703865404ef82498322b6261ff6401fd175116f7
Michael Paler <paler@civicsolar.com>
2020-09-21 13:43:33 -0700

    Move scss variables into own file for global goodness.
    gh-42

---
+++ hashtag-dapp/vue.config.js
@@ -0,0 +1,25 @@
+// vue.config.js
+module.exports = {
+  // Make variables available in SASS for every components
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+            @import '@/assets/scss/variables.scss';
+          `,
+      },
+    },
+  },
+  pluginOptions: {
+    apollo: {
+      lintGQL: true,
+    },
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule("pdf")
+      .test(/\.pdf$/)
+      .use("file-loader")
+      .loader("file-loader");
+  },
+};
