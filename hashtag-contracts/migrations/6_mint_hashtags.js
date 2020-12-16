698f9aa798358803a61c63e5c7b4203f10006e07
Andy Gray <andy.gray@blockrocket.tech>
2020-12-16 14:44:35 +0000

    new rinkeby deployment

--- hashtag-contracts/migrations/6_mint_hashtags.js
+++ hashtag-contracts/migrations/6_mint_hashtags.js
@@ -9,16 +9,11 @@ module.exports = async function (deployer, network, accounts) {
     console.log('Adding publisher access controls to network: ' + network);
 
     const creator = getAccountAddress(accounts, 0, network, MNEMONIC, INFURA_KEY);
-    const alice = getAccountAddress(accounts, 1, network, MNEMONIC, INFURA_KEY);
-    const bob = getAccountAddress(accounts, 2, network, MNEMONIC, INFURA_KEY);
 
     const hashtagProtocol = await HashtagProtocol.deployed();
 
-    await hashtagProtocol.mint("blockrocket", creator, creator, {from: creator});
-    await hashtagProtocol.mint("manchester", creator, alice, {from: alice});
-    await hashtagProtocol.mint("alice", creator, alice, {from: alice});
-    await hashtagProtocol.mint("blockchain", creator, bob, {from: creator});
-    await hashtagProtocol.mint("bob", creator, bob, {from: creator});
+    await hashtagProtocol.mint("#BlockRocket", creator, creator, {from: creator});
+    await hashtagProtocol.mint("#HashtagProtocol", creator, creator, {from: creator});
 
     console.log('successful!');
 };
