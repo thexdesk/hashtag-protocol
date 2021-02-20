f78527e97d597933716b590a4fce2ffbac8f56f8
Michael Paler <paler@civicsolar.com>
2021-02-19 16:02:07 -0800

    Place refactored header and navbar in views.
    gh-110

--- hashtag-dapp/src/views/Publishers.vue
+++ hashtag-dapp/src/views/Publishers.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="body">
-    <section class="hero has-background-grey-dark is-bold">
-      <div class="hero-head">
-        <div class="container">
-          <Header></Header>
-        </div>
-      </div>
-    </section>
+    <Header />
     <section class="main">
       <div class="container">
         <h1 class="title is-1">Publishers</h1>
