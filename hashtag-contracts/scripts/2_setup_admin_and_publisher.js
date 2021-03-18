fc0c286d450d86704d458f5e3f435fd41c56a63b
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-03-18 11:29:15 +0000

    script bug fix

--- hashtag-contracts/scripts/2_setup_admin_and_publisher.js
+++ hashtag-contracts/scripts/2_setup_admin_and_publisher.js
@@ -22,7 +22,7 @@ async function main() {
     deployer //provider
   );
 
-  const DEFAULT_ADMIN_ROLE = '0x00'
+  const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000'
   await accessControls.grantRole(DEFAULT_ADMIN_ROLE, '0x82D08d80ccAC6263018Df22e8E3B6B3c7e925491');
   await accessControls.grantRole(web3.utils.sha3('PUBLISHER'), '0xF6423a8769292BBeef9335C5A26254C759E3BFBe');
 
