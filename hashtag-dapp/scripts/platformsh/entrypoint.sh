53e3e348c7717d91e275903d6e7a3f8de06a1fe7
Michael Paler <paler@civicsolar.com>
2021-03-20 08:34:00 -0700

    Remove unused entrypoint.sh script.
    gh-160

--- hashtag-dapp/scripts/platformsh/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-JSON_STRING='window.configs = { \
-  "VUE_APP_ONBOARD_NETWORK_ID":"'"${VUE_APP_ONBOARD_NETWORK_ID}"'", \
-  "VUE_APP_HASHTAG_SUBGRAPH_URL":"'"${VUE_APP_HASHTAG_SUBGRAPH_URL}"'" \
-}'
-
-sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /app/build_platform/index.html
-exec "$@"
