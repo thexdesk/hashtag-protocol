2820bfaca0d87c1df09735409fdd7b95b6ee5265
Michael Paler <paler@civicsolar.com>
2021-03-19 22:24:54 -0700

    Different env variables for dev and master.
    gh-160

--- hashtag-dapp/src/App.vue
+++ hashtag-dapp/src/App.vue
@@ -2,18 +2,19 @@
   <div id="app">
     <div>{{ variable1 }}</div>
     <div>{{ variable2 }}</div>
+    <div>{{ variable3 }}</div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import getEnv from "@/utils/env";
 export default {
   name: "App",
   data() {
     return {
-      variable1: getEnv("VUE_APP_ONBOARD_NETWORK_ID"),
-      variable2: getEnv("VUE_APP_HASHTAG_SUBGRAPH_URL"),
+      variable1: this.$appConfig.hashtagSubgraph,
+      variable2: this.$appConfig.nftSearchSubgraph,
+      variable3: process.env.NODE_ENV,
     };
   },
   components: {},
