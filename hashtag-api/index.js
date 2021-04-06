7e9a0d555e29c848f16c8ac2760f65e62ec7383f
Michael Paler <paler@civicsolar.com>
2021-04-06 10:40:23 -0700

    Use puppeteer service on platform.sh.
    gh-182

--- hashtag-api/index.js
+++ hashtag-api/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const moment = require("moment");
 const config = require("platformsh-config").config();
-const nodeHtmlToImage = require("node-html-to-image");
+//const nodeHtmlToImage = require("node-html-to-image");
 
 let PORT;
 if (!config.isValidPlatform()) {
@@ -13,9 +13,9 @@ if (!config.isValidPlatform()) {
 }
 
 const app = express().set("port", PORT);
-
-// Static public files
 app.use(express.static(path.join(__dirname, "public")));
+// Disable favicon.
+app.get("/favicon.ico", (req, res) => res.status(204));
 
 app.get("/:token_id", async function (req, res) {
   // Parse the token id from the URL.
@@ -24,7 +24,20 @@ app.get("/:token_id", async function (req, res) {
     return;
   }
 
-  await axios({
+  buildMetadata(tokenId, req)
+    .then((metadata) => {
+      console.log("build metadata", metadata);
+      res.send(metadata);
+    })
+    .catch((e) => console.log(e));
+});
+
+async function buildMetadata(tokenId, req) {
+  if (!tokenId) {
+    return;
+  }
+  console.log("tokenId", tokenId);
+  let response = await axios({
     url:
       "https://api.thegraph.com/subgraphs/name/hashtag-protocol/hashtag-mainnet",
     method: "post",
@@ -40,66 +53,22 @@ app.get("/:token_id", async function (req, res) {
       }
      }`,
     },
-  })
-    .then((result) => {
-      // console.log("axios result", result.data.data.hashtag);
-      // Get the data returned from The Graph.
-      let hashtag = result.data.data.hashtag;
-      // Stitch in nicely formatted token creation date.
-      hashtag.date = moment.unix(hashtag.timestamp).format("MMM Do YYYY");
-      // Build the token image, and add it to the hashtag object.
-      hashtag = buildImage(hashtag);
-      // Return the hashtag, to be picked up in next "then".
-      console.log("build image", hashtag);
-      return hashtag;
-    })
-    .then((result) => {
-      // result is hashtag json object with image element added in previous step.
-      // Build the token metadata structure.
-      const metadata = buildMetadata(result, req);
-      console.log("build metadata", hashtag);
-      // send to the screen.
-      res.send(metadata);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-});
+  });
 
-async function buildImage(hashtag) {
-  // Use es6-template-strings to parse hashtag data
-  // into a template, and return as a string
-  // to pass to the nodeHtmlToImage processor.
-  const fs = require("fs");
-  const compile = require("es6-template-strings/compile"),
-    resolveToString = require("es6-template-strings/resolve-to-string");
-  const data = fs.readFileSync("./assets/templates/series2021.txt", "utf8");
-  const compiled = compile(data);
-  const content = resolveToString(compiled, hashtag);
-
-  try {
-    await nodeHtmlToImage({
-      html: content,
-      output: `./public/images/${hashtag.id}.png`,
-      puppeteerArgs: {
-        // Pass in puppeteer custom options to fix image resolution.
-        // @see
-        // https://github.com/puppeteer/puppeteer/blob/8370ec88ae94fa59d9e9dc0c154e48527d48c9fe/docs/api.md#puppeteerlaunchoptions
-        // https://github.com/puppeteer/puppeteer/issues/571
-        defaultViewport: { deviceScaleFactor: 2, width: 1200, height: 1200 },
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    return false;
+  //console.log("axios response", response);
+  if (response.statusText != "OK") {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
 
-  hashtag["image"] = `images/${hashtag.id}.png`;
-  return hashtag;
-}
+  let hashtag = response.data.data.hashtag;
+  console.log("axios data", response.data.data.hashtag);
 
-function buildMetadata(hashtag, req) {
+  // Stitch in nicely formatted token creation date.
+  hashtag.date = moment.unix(hashtag.timestamp).format("MMM Do YYYY");
+  hashtag.image = await buildImage(hashtag);
+
   const fullUrl = req.protocol + "://" + req.get("host");
+
   const metadata = {
     name: hashtag.displayHashtag,
     external_url: `https://app.hashtag-protocol.org/hashtag/${hashtag.hashtagWithoutHash}`,
@@ -107,6 +76,53 @@ function buildMetadata(hashtag, req) {
   };
 
   return metadata;
+}
+
+async function buildImage(hashtag) {
+  try {
+    // Use es6-template-strings to parse hashtag data
+    // into a template, and return as a string
+    // to pass to the nodeHtmlToImage processor.
+    const fs = require("fs");
+    const compile = require("es6-template-strings/compile"),
+      resolveToString = require("es6-template-strings/resolve-to-string");
+    const data = fs.readFileSync("./assets/templates/series2021.txt", "utf8");
+    const compiled = compile(data);
+
+    const html = resolveToString(compiled, hashtag);
+    const path = `./public/images/${hashtag.id}.png`;
+    const puppeteer = require("puppeteer");
+
+    let browser;
+    // Connect to chrome-headless using pre-formatted puppeteer credentials
+    if (config.isValidPlatform()) {
+      const formattedURL = config.formattedCredentials("headless", "puppeteer");
+      browser = await puppeteer.connect({ browserURL: formattedURL });
+    } else {
+      browser = await puppeteer.launch({ headless: true });
+    }
+
+    let page = await browser.newPage();
+    const loaded = page.waitForNavigation({
+      waitUntil: "load",
+    });
+    await page.setContent(html, {
+      waitUntil: ["domcontentloaded", "networkidle2"],
+    });
+    await loaded;
+    await page.setViewport({ width: 600, height: 600, deviceScaleFactor: 2 });
+    await page.screenshot({
+      path: path,
+      type: "png",
+      fullPage: true,
+    });
+    await page.close();
+    await browser.close();
+
+    return `images/${hashtag.id}.png`;
+  } catch (e) {
+    return Promise.reject(e);
+  }
 }
 
 // Get PORT and start the server
