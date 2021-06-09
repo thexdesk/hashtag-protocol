8541279d7ec10dace35de5f3af249146af34af64
swaylock <admin@swaylocks.com>
2021-06-09 09:30:51 -0700

    Pull FAQ .md file directly in from filesystem for help bubbles.
    gh-190

--- hashtag-dapp/src/components/MarkdownDoc.vue
+++ hashtag-dapp/src/components/MarkdownDoc.vue
@@ -2,7 +2,6 @@
   <div v-html="docBody" class="content"></div>
 </template>
 <script>
-//const axios = require("axios");
 const md = require("markdown-it")();
 /**
  * Load up a raw markdown file from the hashtag-docs directory,
@@ -26,30 +25,8 @@ export default {
   },
   methods: {
     async getDoc() {
-      let markdown = require(`../../../hashtag-docs/docs/shared/faqs/${this.filename}.md`);
-      this.docBody = md.render(markdown);
-      console.log("MarkdownDoc", this.docBody);
-      //axios
-      //  .get(`/docs/${this.filename}.md`)
-      //  .then((response) => {
-      //    this.docBody = md.render(response.data);
-      //  })
-      //  .catch((error) => {
-      //    if (error.response) {
-      //      // client received an error response (5xx, 4xx)
-      //      this.docBody =
-      //        "Error parsing document response: " +
-      //        error.response.status +
-      //        " " +
-      //        error.response.statusText;
-      //    } else if (error.request) {
-      //      // client never received a response, or request never left
-      //      this.docBody = "Error requesting document : " + error.response;
-      //    } else {
-      //      // anything else
-      //      this.docBody = "Error parsing document.";
-      //    }
-      //  });
+      let markdown = require(`raw-loader!../../../hashtag-docs/docs/shared/faqs/${this.filename}.md`);
+      this.docBody = md.render(markdown.default);
     },
   },
 };
