2433f496037c0ee67a3c10b01b848e53334d24ba
Zach Williams <zawilliams@gmail.com>
2021-06-29 16:42:55 -0500

    Remove old layout cruft

--- hashtag-dapp/layouts/default.vue
+++ hashtag-dapp/layouts/default.vue
@@ -1,27 +1,5 @@
 <template>
   <div id="app">
-    <router-view></router-view>
-    <div v-if="variable3 == 'development'" class="is-size-7">
-      <div>{{ variable1 }}</div>
-      <div>{{ variable2 }}</div>
-      <div>Build type: {{ variable3 }}</div>
-    </div>
+    <Nuxt />
   </div>
 </template>
-
-<script>
-export default {
-  name: "App",
-  data() {
-    return {
-      variable1: this.$appConfig.hashtagSubgraph,
-      variable2: this.$appConfig.nftSearchSubgraph,
-      variable3: process.env.NODE_ENV,
-    };
-  },
-  components: {},
-};
-</script>
-<style lang="scss">
-@import "./styles/themes/hashtag/theme";
-</style>
