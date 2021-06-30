a3d6821074387afb264b270fc429d6fce3278bf2
Zach Williams <zawilliams@gmail.com>
2021-06-30 16:43:46 -0500

    Remove unneeded global mixin

--- hashtag-dapp/nuxt.config.js
+++ hashtag-dapp/nuxt.config.js
@@ -32,7 +32,6 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    "~/plugins/global-mixin.client",
     "~/plugins/vue-axios",
     "~/plugins/vue-buefy",
     "~/plugins/vue-filter",
